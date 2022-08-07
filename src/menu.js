7bc5239b4de1ab89994b6daf827162ae9a84a15a
Anrii Sereda <a.a.sereda@ukr.net>
2022-08-07 13:37:17 +0300

    add header style

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-  //   const mobileMenuRefNav = document.querySelector('[data-menu-nav]');
+  const mobileMenuRefPurchase = document.querySelector('[data-menu-purchase]');
   //   const mobileMenuRefCont = document.querySelector('[data-menu-cont]');
   //   const mobileMenuRefSoc = document.querySelector('[data-menu-social]');
 
@@ -12,7 +12,7 @@
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
-    // mobileMenuRefNav.classList.toggle('is-open');
+    mobileMenuRefPurchase.classList.toggle('is-open');
     // mobileMenuRefCont.classList.toggle('is-open');
     // mobileMenuRefSoc.classList.toggle('is-open');
     document.body.classList.toggle('menu-open');
