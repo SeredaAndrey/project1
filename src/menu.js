43941cdc618479b2924e46359d3d12c415dba331
Anrii Sereda <a.a.sereda@ukr.net>
2022-08-07 23:44:07 +0300

    add mobile menu stile

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,7 @@
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
   const mobileMenuRefPurchase = document.querySelector('[data-menu-purchase]');
-  //   const mobileMenuRefCont = document.querySelector('[data-menu-cont]');
+  const mobileMenuRefNav = document.querySelector('[data-menu-nav]');
   //   const mobileMenuRefSoc = document.querySelector('[data-menu-social]');
 
   menuBtnRef.addEventListener('click', () => {
@@ -13,7 +13,7 @@
     menuBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
     mobileMenuRefPurchase.classList.toggle('is-open');
-    // mobileMenuRefCont.classList.toggle('is-open');
+    mobileMenuRefNav.classList.toggle('is-open');
     // mobileMenuRefSoc.classList.toggle('is-open');
     document.body.classList.toggle('menu-open');
   });
