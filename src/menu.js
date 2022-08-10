313b2910d1599013b2b8b742dbfc007d6441a5af
Anrii Sereda <a.a.sereda@ukr.net>
2022-08-10 12:54:30 +0300

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -3,18 +3,16 @@
   const mobileMenuRef = document.querySelector('[data-menu]');
   const mobileMenuRefPurchase = document.querySelector('[data-menu-purchase]');
   const mobileMenuRefNav = document.querySelector('[data-menu-nav]');
-  //   const mobileMenuRefSoc = document.querySelector('[data-menu-social]');
 
+  const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+  let dataMenuToggle = true;
   menuBtnRef.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
     mobileMenuRefPurchase.classList.toggle('is-open');
     mobileMenuRefNav.classList.toggle('is-open');
-    // mobileMenuRefSoc.classList.toggle('is-open');
     document.body.classList.toggle('menu-open');
+    dataMenuToggle = true;
   });
 })();
